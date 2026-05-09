f1e81f39ef3daad9a20a5f957ae97aa0f66e7e96
luanaruffinelli <luanaruffinelli02@proton.me>
2026-05-08 21:25:15 -0300

    coneccion db

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  )
+
+
   await app.listen(process.env.PORT ?? 3000);
+
+  Logger.log(`Server is running on port ${await app.getUrl()}`);
 }
 bootstrap();
