412dfe379fca9f304481270a86c1ebff1268a0bc
luanaruffinelli <luanaruffinelli02@proton.me>
2026-06-08 22:04:28 -0300

    feat: add ProductsModule and ProvidersModule to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
+import { ProvidersModule } from './providers/providers.module';
 
 @Module({
-  imports: [UsersModule, CategoriesModule],
+  imports: [UsersModule, CategoriesModule, ProductsModule, ProvidersModule],
   controllers: [AppController],
   providers: [AppService],
 })
